517341d253d7e347d424b2483e0939c573e6fd79
Cartel-80 <joshuamulenga96@hotmail.com>
2023-03-26 16:19:26 -0300

    initial commit again

--- src/main/java/com/j11a/async/AsyncApplication.java
+++ src/main/java/com/j11a/async/AsyncApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AsyncApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AsyncApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AsyncApplication.class, args);
+    }
 
 }
